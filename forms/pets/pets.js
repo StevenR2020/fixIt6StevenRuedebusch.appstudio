2e713deb63daaf13fea7a838d943b7c516d1b6f0
sruedebusch <54645645+sruedebusch@users.noreply.github.com>
2020-02-09 16:31:43 -0600

    Update pets.js

--- forms/pets/pets.js
+++ forms/pets/pets.js
@@ -1,36 +1,108 @@
-let petNames = ["Darcy", "Jack", "Jerry", "Marty", "Vito", "Vinny", "Riley", "Saddie", "Marge", "Sam"]
-let withMarmaduke = petNames.push("Marmaduke")
-console.log(petNames, withMarmaduke)
+let petNames= ["Darcy", "Jack", "Jerry", "Marty", "Vito", "Vinny", "Riley", "Saddie", "Marge", "Sam"]
+let withMarmaduke= petNames.push("Marmaduke")
+console.log(`The pet names are ${petNames}.`)
+
+let userChoice = prompt(`Please choose the action you want by entering the corresponding number:
+
+1. Remove Vinny from the array.
+2. Remove Marty from the array.
+3. Add Nancy to the front of the array.
+4. Add your name to the end of the array.
+5. Find out where Riley is located in the array.
+6. Find out where Cindy is located in the array.
+7. Show the withMarmaduke array data.`)
+
+switch(userChoice) {
+  case'1':
+    let removeVinny= petNames.splice(5,1)
+    alert(`The action taken was: Remove Vinny
+    The remaining data are: ${petNames}`)
+    console.log('The action taken was: Remove Vinny')
+    console.log('The remaining data are ' + petNames)
+    break;
+    
+  case'2':
+    let removeMarty = petNames.splice(3,1)
+    alert(`The action taken was: Remove Marty
+    The remaining data are: ${petNames}`)
+    console.log('The action taken was: Remove Marty')
+    console.log('The remaining data are ' + petNames)
+    break;
+    
+  case'3':
+    let addNancy= petNames.unshift(Nancy)
+    alert(`The action taken was: Add Nancy
+    The remaining data are: ${petNames}`)
+    console.log('The action taken was: Add Nancy')
+    console.log('The remaining data are ' + petNames)
+    break;
+    
+  case'4':
+    let addUser = petNames.push(Steven)
+    alert(`The action taken was: Add user name
+    The remaining data are: ${petNames}`)
+    console.log('The action taken was: Add User')
+    console.log('The remaining data are ' + petNames)
+    break;
+    
+  case'5':
+    rileyIndex = petNames.indexOf('Riley')
+    alert(`The action taken was: Find Riley
+    The index of riley is ${rileyIndex}`)
+    console.log('The action taken was: Find the index of Riley')
+    console.log('The remaining data are ' + petNames)
+    break;
+  case'6':
+    cindyIndex = petNames.indexOf('Cindy')
+    alert(`The action taken was: Find Cindy
+    The index of Cindy is ${cindyIndex}`)
+    console.log('The action taken was: Find the index of Cindy')
+    console.log('The remaining data are ' + petNames)
+    break;
+    
+  case'7':
+    alert(`The array data for withMarmaduke is ${withMarmaduke}`)
+    console.log('The action taken was: Find the array data for withMarmaduke variable')
+    console.log('The remaining data are ' + petNames)
+    break;
+  
+  default:
+    alert(`Invalid input.`)
+    break;
+  }
 
 
-// These lines of code is removing the 5th element and only 1 element from the array.
-petNames.splice(5,1)
+
+// Scenario AB
+
+petNames.shift()
 console.log(petNames)
-console.log("The action taken was: Remove Vinny")
-console.log("The remaining data are: [Darcy, Jack, Jerry, Marty, Vito, Riley, Saddie, Marge, Sam]")
 
-// These lines of code is removing the 3rd element and only 1 element from the array.
-petNames.splice(3,1)
+petNames.unshift("Barney")
 console.log(petNames)
-console.log("The action taken was: Remove Marty")
-console.log("The remaining data are: [Darcy, Jack, Jerry, Vito, Riley, Saddie, Marge, Sam]")
 
-// These lines of code are adding the name Nancy to the beginning of the list.
-petNames.unshift("Nancy")
+let allPets = petNames.push(["Ted", "Fred", "Jed", "Ned", "Ed", "Zed"])
+console.log(allPets)
+
+let stringPets = allPets.join(["Bob", "Sam", "Andy"])
+console.log(stringPets)
+
+petNames.unshift("Agnes")
 console.log(petNames)
-console.log("The action taken was: Add Nancy")
-console.log("The remaining data are: [Nancy, Darcy, Jack, Jerry, Vito, Riley, Saddie, Marge, Sam]")
 
-// These lines of code are adding the user's name to the end of the list.
-petNames.push("Steven")
-console.log(petNames)
-console.log("The action taken was: Add user's name")
-console.log("The remaining data are: [Nancy, Darcy, Jack, Jerry, Vito, Riley, Saddie, Marge, Sam, Steven]")
 
-// These lines of code are informing the user of where the name "Riley" is located in the list.
-rileyIndex = petNames.indexOf("Riley")
-console.log(rileyIndex)
 
-// These lines of code are informing the user of where the name "Cindy" is located in the list.
-cindyIndex = petNames.indexOf("Cindy")
-console.log(cindyIndex)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
